82eb70329815557fecea4e5173c2922eadf57feb
MonsieurSadie <kevin.lesur@gmail.com>
2021-01-25 20:43:53 +0100

    added some code

--- Assets/BranchScript.cs
+++ Assets/BranchScript.cs
@@ -7,7 +7,7 @@ public class BranchScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("do smth in code");
     }
 
     // Update is called once per frame
