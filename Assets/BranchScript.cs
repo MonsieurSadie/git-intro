cb094ea73251d47ac71958ab4f4505ae8c2d9469
MonsieurSadie <kevin.lesur@gmail.com>
2023-03-30 09:38:41 +0200

    Update BranchScript.cs

--- Assets/BranchScript.cs
+++ Assets/BranchScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BranchScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first aya  frame update
     void Start()
     {
         Debug.Log("do smth in code");
